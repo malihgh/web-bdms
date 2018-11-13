91cd3a29685cba7f8229a53f2eeee15504a5c295
Milan P. Antonovic <milan.antonovic@gmail.com>
2018-11-13 15:08:13 +0100

    disabled serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import {
   createStore,
   applyMiddleware,
-} from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { I18nextProvider } from 'react-i18next'
-import i18n from './i18n'
-import './index.css'
-import 'ol/ol.css'
-import App from './App'
+} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+// import './index.css'
+import 'ol/ol.css';
+import App from './App';
 // import registerServiceWorker from './registerServiceWorker'
-import 'semantic-ui-css/semantic.css'
+import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 
-import {appReducers} from './reducers'
-
-import {createReducer} from '@ist-supsi/bmsjs'
-
+import 'semantic-ui-css/semantic.css';
+import {appReducers} from './reducers';
+import {createReducer} from '@ist-supsi/bmsjs';
 
 const store = createStore(
   createReducer(appReducers),
@@ -25,7 +24,7 @@ const store = createStore(
   applyMiddleware(
     thunkMiddleware
   )
-)
+);
 
 ReactDOM.render(
   <I18nextProvider i18n={ i18n }>
@@ -34,5 +33,6 @@ ReactDOM.render(
     </Provider>
   </I18nextProvider>,
   document.getElementById('root')
-)
-// registerServiceWorker()
+);
+// registerServiceWorker();
+unregisterServiceWorker();
