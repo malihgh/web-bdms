4b9c353f5ee6a9658bf4ff6aab73d90b7da07d98
Milan P. Antonovic <milan.antonovic@gmail.com>
2019-10-02 16:56:41 +0200

    refactoring

--- src/commons/form/domain/dropdown/domainDropdown.js
+++ src/commons/form/domain/dropdown/domainDropdown.js
@@ -24,12 +24,32 @@ class DomainDropdown extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
+  static getDerivedStateFromProps(nextProps, prevState){
+    const state = { ...prevState };
+    if (_.isNil(nextProps.selected)){
+      if (nextProps.multiple === true) {
+        state.selected = [];
+      } else {
+        state.selected = null;
+      }
+    } else {
+      state.selected = nextProps.selected;
+    }
+    if (nextProps.i18n.language !== prevState.language){
+      state.language = nextProps.i18n.language;
+    }
+    if (_.isEqual(state, prevState)) {
+      return null;
+    }
+    return state;
+  }
+
   componentDidMount(){
     const {
       domains,
       schema
     } = this.props;
-    if(!domains.data.hasOwnProperty(schema) && domains.isFetching === false){
+    if (!domains.data.hasOwnProperty(schema) && domains.isFetching === false){
       this.props.loadDomains();
     }
   }
@@ -43,24 +63,6 @@ class DomainDropdown extends React.Component {
     return false;
   }
 
-  static getDerivedStateFromProps(nextProps, prevState){
-    const state = {...prevState};
-    if (_.isNil(nextProps.selected)){
-      if(nextProps.multiple === true) {
-        state.selected = [];
-      } else {
-        state.selected = null;
-      }
-    } else {
-      state.selected = nextProps.selected;
-    }
-    if (nextProps.i18n.language !== prevState.language){
-      state.language = nextProps.i18n.language;
-    }
-    if (_.isEqual(state, prevState)) return null;
-    return state;
-  }
-
   handleChange(event, data) {
     const {
       onSelected,
@@ -68,36 +70,36 @@ class DomainDropdown extends React.Component {
       schema,
       multiple
     } = this.props;
-    if(multiple===true){
+    if (multiple===true){
       const selection = [];
       for (let i = 0; i < domains.data[schema].length; i++) {
         let h = domains.data[schema][i];
         for (var f = 0; f < data.value.length; f++) {
           const s = data.value[f];
-          if(h.id === s){
-            selection.push({...h});
+          if (h.id === s){
+            selection.push({ ...h });
           }
         }
       }
-      this.setState({selected: data.value});
-      if(onSelected!==undefined){
+      this.setState({ selected: data.value });
+      if (onSelected!==undefined){
         onSelected(selection);
       }
-    }else{
-      if(data.value === null){
-        this.setState({selected: null})
-        if(onSelected!==undefined){
+    } else {
+      if (data.value === null){
+        this.setState({ selected: null });
+        if (onSelected!==undefined){
           onSelected({
             id: null
           });
         }
-      }else{
+      } else {
         for (let i = 0; i < domains.data[schema].length; i++) {
           let h = domains.data[schema][i];
-          if(h.id === data.value){
-            this.setState({selected: h.id});
-            if(onSelected!==undefined){
-              onSelected({...h});
+          if (h.id === data.value){
+            this.setState({ selected: h.id });
+            if (onSelected!==undefined){
+              onSelected({ ...h });
             }
             break;
           }
@@ -115,13 +117,13 @@ class DomainDropdown extends React.Component {
       multiple
     } = this.props, {
         selected
-    } = this.state;
-    if(!domains.data.hasOwnProperty(schema)){
-      if(domains.isFetching === true){
+      } = this.state;
+    if (!domains.data.hasOwnProperty(schema)){
+      if (domains.isFetching === true){
         return 'loading translations';
       }
       return (
-        <div style={{color: 'red'}}>
+        <div style={{ color: 'red' }}>
           "{schema}" not in codelist
         </div>
       );
@@ -132,13 +134,15 @@ class DomainDropdown extends React.Component {
         key: "dom-opt-z",
         value: null,
         text: '',
-        content: <span
-          style={{
-            color: 'red'
-          }}  
-        >
-          {t('reset')}
-        </span>
+        content: (
+          <span
+            style={{
+              color: 'red'
+            }}  
+          >
+            {t('reset')}
+          </span>
+        )
       });
     }
     options = _.concat(options, domains.data[schema].map((domain) => ({
@@ -148,72 +152,75 @@ class DomainDropdown extends React.Component {
         domain[this.state.language].text:
         domain.code !== domain[this.state.language].text?
           domain.code + ' (' + domain[this.state.language].text + ')': domain.code,
-      content: <Header
-        content={
-          <div
-            style={{
-              borderLeft: domain.conf !== null?
-                domain.conf.hasOwnProperty('color')?
-                  '1em solid rgb(' +
-                  domain.conf.color[0] + ", " +
-                  domain.conf.color[1] + ", " +
-                  domain.conf.color[2] + ")": null
-                : null,
-              paddingLeft: domain.conf !== null?
-                domain.conf.hasOwnProperty('color')?
-                  '0.5em': null
-                : null
-            }}
-          >
-            {
+      content: (
+        <Header
+          content={
+            <div
+              style={{
+                borderLeft: domain.conf !== null?
+                  domain.conf.hasOwnProperty('color')?
+                    '1em solid rgb(' +
+                    domain.conf.color[0] + ", " +
+                    domain.conf.color[1] + ", " +
+                    domain.conf.color[2] + ")": null
+                  : null,
+                paddingLeft: domain.conf !== null?
+                  domain.conf.hasOwnProperty('color')?
+                    '0.5em': null
+                  : null
+              }}
+            >
+              {
+                domain.code === ''?
+                  domain[this.state.language].text:
+                  domain.code
+              }
+            </div>
+          }
+          style={{
+            backgroundImage: domain.conf !== null?
+              domain.conf.hasOwnProperty('img')?
+                'url("' + process.env.PUBLIC_URL + '/img/lit/' +
+                domain.conf.img + '")': null
+              : null
+          }}
+          subheader={
+            domain[
+              this.state.language
+            ].descr !== null
+            && domain[
+              this.state.language
+            ].descr !== ''?
+              domain[this.state.language].text + ', ' +  domain[this.state.language].descr:
               domain.code === ''?
-                domain[this.state.language].text:
-                domain.code
-            }
-          </div>
-        }
-        style={{
-          backgroundImage: domain.conf !== null?
-            domain.conf.hasOwnProperty('img')?
-              'url("' + process.env.PUBLIC_URL + '/img/lit/' +
-              domain.conf.img + '")': null
-            : null
-        }}
-        subheader={
-          domain[
-            this.state.language
-          ].descr !== null
-          && domain[
-            this.state.language
-          ].descr !== ''?
-            domain[this.state.language].text + ', ' +  domain[this.state.language].descr:
-            domain.code === ''?
-              null: domain[this.state.language].text
-        }
-      />
+                null: domain[this.state.language].text
+          }
+        />
+      )
     })));
     return (
       <Form.Select
-        fluid={true}
-        search={search}
+        fluid
         multiple={multiple}
+        onChange={this.handleChange}
         options={options}
+        search={search}
         value={selected}
-        onChange={this.handleChange}/>
+      />
     );
   }
 };
 
 DomainDropdown.propTypes = {
+  multiple: PropTypes.bool,
+  onSelected: PropTypes.func,
+  reset: PropTypes.bool,
   schema: PropTypes.string,
+  search: PropTypes.bool,
   selected: PropTypes.oneOfType([
     PropTypes.number,
     PropTypes.arrayOf(PropTypes.number)
-  ]),
-  onSelected: PropTypes.func,
-  search: PropTypes.bool,
-  multiple: PropTypes.bool,
-  reset: PropTypes.bool
+  ])
 };
 
 DomainDropdown.defaultProps = {
@@ -241,5 +248,5 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )((
-   translate('common')(DomainDropdown)
+  translate('common')(DomainDropdown)
 ));
