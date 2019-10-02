6a159b3e0bf2464807094d8c50927164c4d87d3d
Milan P. Antonovic <milan.antonovic@gmail.com>
2019-10-02 15:31:49 +0200

    small refactoring

--- src/pages/settings/settingCmp.js
+++ src/pages/settings/settingCmp.js
@@ -13,74 +13,62 @@ import ExplorerSettings from './explorerSettings';
 import EditorSettings from './editorSettings';
 import AdminSettings from './adminSettings';
 
-// const SettingCmp = (props) => {
-class SettingCmp extends React.Component {
-
-  render() {
-    return (
+const SettingCmp = (props) => {
+  
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%'
+      }}
+    >
+      <MenuContainer />
       <div
         style={{
+          flex: '1 1 100%',
+          // height: '100%',
           display: 'flex',
-          flexDirection: 'column',
-          height: '100%'
+          flexDirection: 'row',
+          overflow: 'hidden'
         }}
       >
-        <MenuContainer />
         <div
           style={{
-            flex: '1 1 100%',
-            // height: '100%',
+            // borderRight: 'thin solid #dfe0e0',
+            boxShadow: 'rgba(0, 0, 0, 0.17) 2px 6px 6px 0px',
             display: 'flex',
-            flexDirection: 'row',
-            overflow: 'hidden'
+            flexDirection: 'column',
+            width: '250px'
           }}
         >
-          <div
-            style={{
-              // borderRight: 'thin solid #dfe0e0',
-              boxShadow: 'rgba(0, 0, 0, 0.17) 2px 6px 6px 0px',
-              display: 'flex',
-              flexDirection: 'column',
-              width: '250px'
-            }}
-          >
-            <MenuSettings />
-          </div>
-          <div
-            style={{
-              flex: '1 1 0%',
-              overflowY: 'auto'
-            }}
-          >
-            <Switch>
-              <Route
-                component={ExplorerSettings}
-                path={process.env.PUBLIC_URL + "/setting/explorer"}
-              />
-              <Route
-                component={EditorSettings}
-                path={process.env.PUBLIC_URL + "/setting/editor"}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/setting/admin"}
-                component={AdminSettings}
-                // render={() => (
-                //   <div
-                //     style={{
-                //       padding: '2em',
-                //       flex: 1
-                //     }}
-                //   >
-                //     coming soon
-                //   </div>
-                // )}
-              />
-            </Switch>
-          </div>
+          <MenuSettings />
+        </div>
+        <div
+          style={{
+            flex: '1 1 0%',
+            overflowY: 'auto'
+          }}
+        >
+          <Switch>
+            <Route
+              component={ExplorerSettings}
+              path={process.env.PUBLIC_URL + "/setting/explorer"}
+            />
+            <Route
+              component={EditorSettings}
+              path={process.env.PUBLIC_URL + "/setting/editor"}
+            />
+            <Route
+              component={AdminSettings}
+              path={process.env.PUBLIC_URL + "/setting/admin"}
+            />
+          </Switch>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+  // }
 };
 
 export default withRouter(translate('common')(SettingCmp));
