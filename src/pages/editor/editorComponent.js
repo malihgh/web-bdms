40a4482a9c6e5eafbe01227a55dd361c6c77b4e1
Antonovic P. Milan <milan.antonovic@gmail.com>
2021-10-14 09:39:17 +0200

    Map in editor page

--- src/pages/editor/editorComponent.js
+++ src/pages/editor/editorComponent.js
@@ -19,6 +19,7 @@ import {
 import _ from 'lodash';
 
 import BoreholeForm from '../../commons/form/borehole/boreholeForm';
+import MapComponent from '../../commons/map/mapComponent';
 import BoreholeEditorTable from '../../commons/table/boreholeEditorTable';
 import MenuEditorSearch from '../../commons/menu/editor/menuEditorSearch';
 import MenuEditorForm from '../../commons/menu/editor/menuEditorForm';
@@ -27,166 +28,248 @@ import WorkflowForm from '../../commons/form/workflow/workflowForm';
 import MultipleForm from '../../commons/form/multiple/multipleForm';
 
 
-const EditorComponent = function (props) {
+// const EditorComponent = function (props) {
 
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%'
-      }}
-    >
-      <MenuContainer />
-      <div
-        style={{
-          flex: '1 1 100%',
-          display: 'flex',
-          flexDirection: 'row',
-          overflow: 'hidden'
-        }}
-      >
+class EditorComponent extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.rowHover = null;
+      this.state = {
+        hover: null,
+        maphover: null,
+      };
+    }
+
+    render() {
+      const props = this.props;
+
+      return (
         <div
           style={{
-            // borderRight: 'thin solid #dfe0e0',
-            boxShadow: 'rgba(0, 0, 0, 0.17) 2px 6px 6px 0px',
             display: 'flex',
             flexDirection: 'column',
-            width: '250px'
+            height: '100%'
           }}
         >
-          <Switch>
-            <Route
-              component={MenuEditorSearch}
-              exact
-              path={process.env.PUBLIC_URL + "/editor"}
-            />
-            <Route
-              component={MenuEditorForm}
-              // onTimeout={()=>{
-
-              // }}
-              path={process.env.PUBLIC_URL + "/editor/:id"}
-            />
-          </Switch>
-        </div>
-
-        <div
-          style={{
-            flex: '1 1 0%',
-            display: 'flex',
-            flexDirection: 'row'
-          }}
-        >
-          <Switch>
-            <Route
-              exact
-              path={process.env.PUBLIC_URL + "/editor"}
-              render={() => (
-                <div
-                  style={{
-                    flex: '1 1.5 100%',
-                    // padding: "1em",
-                    // boxShadow: 'rgba(0, 0, 0, 0.5) 0px 0px 8px 0px inset',
-                    display: 'flex',
-                    flexDirection: 'column'
-                  }}
-                >
-                  <Modal
-                    onUnmount={()=>{
-                      // props.multipleSelected(null);
-                      props.loadEditingBoreholes(
-                        props.editor.page,
-                        props.search.filter,
-                        props.editor.direction
-                      );
-                    }}
-                    // open={props.store.mselected}
-                    open={Array.isArray(props.store.mselected)}
-                  >
-                    <Modal.Content
+          <MenuContainer />
+          <div
+            style={{
+              flex: '1 1 100%',
+              display: 'flex',
+              flexDirection: 'row',
+              overflow: 'hidden'
+            }}
+          >
+            <div
+              style={{
+                // borderRight: 'thin solid #dfe0e0',
+                boxShadow: 'rgba(0, 0, 0, 0.17) 2px 6px 6px 0px',
+                display: 'flex',
+                flexDirection: 'column',
+                width: '250px'
+              }}
+            >
+              <Switch>
+                <Route
+                  component={MenuEditorSearch}
+                  exact
+                  path={process.env.PUBLIC_URL + "/editor"}
+                />
+                <Route
+                  component={MenuEditorForm}
+                  // onTimeout={()=>{
+    
+                  // }}
+                  path={process.env.PUBLIC_URL + "/editor/:id"}
+                />
+              </Switch>
+            </div>
+    
+            <div
+              style={{
+                flex: '1 1 0%',
+                display: 'flex',
+                flexDirection: 'row'
+              }}
+            >
+              <Switch>
+                <Route
+                  exact
+                  path={process.env.PUBLIC_URL + "/editor"}
+                  render={() => (
+                    <div
                       style={{
-                        // maxHeight: '600px',
-                        // overflowY: 'auto'
+                        flex: '1 1.5 100%',
+                        // padding: "1em",
+                        // boxShadow: 'rgba(0, 0, 0, 0.5) 0px 0px 8px 0px inset',
+                        display: 'flex',
+                        flexDirection: 'column'
                       }}
                     >
-                      <MultipleForm 
-                        selected={props.store.mselected}
-                      />
-                    </Modal.Content>
-                  </Modal>
-                  <BoreholeEditorTable
-                    activeItem={
-                      !_.isNil(props.store.bselected) ?
-                        props.store.bselected.id : null
-                    }
-                    filter={{
-                      ...props.search.filter
-                    }}
-                    onDelete={(selection) => {
-                      props.delete(
-                        selection, props.search.filter
-                      );
-                    }}
-                    onMultiple={(selection) => {
-                      props.multipleSelected(
-                        selection, props.search.filter
-                      );
-                    }}
-                    onSelected={(borehole) => {
-                      // Lock borehole
-                      if (borehole !== null) {
-                        props.lock(borehole.id);
-                      }
-                    }}
-                  />
-                </div>
-              )}
-            />
-            <Route
-              exact={false}
-              path={process.env.PUBLIC_URL + "/editor/:id"}
-              render={() => (
-                <div
-                  style={{
-                    overflow: 'hidden',
-                    height: '100%',
-                    display: 'flex',
-                    flex: '1 1 100%',
-                    flexDirection: 'column',
-                    padding: '1em'
-                  }}
-                >
-                  <BoreholeForm />
-                </div>
-              )}
-            />
-          </Switch>
-        </div>
-
-        <Switch>
-          <Route 
-            component={({ match }) => (
-              <div
-                style={{
-                  width: '400px',
-                  boxShadow: 'rgba(0, 0, 0, 0.17) -2px 6px 6px 0px',
-                  padding: '1em'
-                }}
-              >
-                <WorkflowForm
-                  id={
-                    parseInt(match.params.id, 10)
-                  }
+                      <Modal
+                        onUnmount={()=>{
+                          // props.multipleSelected(null);
+                          props.loadEditingBoreholes(
+                            props.editor.page,
+                            props.search.filter,
+                            props.editor.direction
+                          );
+                        }}
+                        // open={props.store.mselected}
+                        open={Array.isArray(props.store.mselected)}
+                      >
+                        <Modal.Content
+                          style={{
+                            // maxHeight: '600px',
+                            // overflowY: 'auto'
+                          }}
+                        >
+                          <MultipleForm 
+                            selected={props.store.mselected}
+                          />
+                        </Modal.Content>
+                      </Modal>
+    
+                      <div
+                        style={{
+                          flex: '1 1 50%',
+                          height: '50%'
+                        }}
+                      >
+                        
+                        <MapComponent
+                          // centerto={
+                          //   search.center2selected
+                          //   && setting.data.appearance.explorer !== 0 ?
+                          //     detail.borehole !== null?  
+                          //       detail.borehole.id : null
+                          //     : null
+                          // }
+                          filter={{
+                            ...props.search.filter
+                          }}
+                          highlighted={
+                            this.state.hover !== null?
+                              [this.state.hover.id] : []
+                          }
+                          hover={(id) => {
+                            console.log(id);
+                            this.setState({
+                              maphover: id,
+                            })
+                          }}
+                          layers={props.setting.data.map.explorer}
+                          // moveend={(features, extent) => {
+                          //   this.props.filterByExtent(extent);
+                          // }}
+                          selected={(id) => {
+                            // this.props.boreholeSeleced(id);
+                            if (id !== null){
+                              props.lock(id);
+                            }
+                          }}
+                          // zoomto={
+                          //   search.zoom2selected
+                          //   && setting.data.appearance.explorer !== 0
+                          // }
+                        />
+                      </div>
+                      <div
+                        style={{
+                          flex: '1 0 50%',
+                          overflowY: 'auto',
+                        }}
+                      >
+                        <BoreholeEditorTable
+                          activeItem={
+                            !_.isNil(props.store.bselected) ?
+                              props.store.bselected.id : null
+                          }
+                          filter={{
+                            ...props.search.filter
+                          }}
+                          highlight={this.state.maphover}
+                          onDelete={(selection) => {
+                            props.delete(
+                              selection, props.search.filter
+                            );
+                          }}
+                          onHover={(item) => {
+                            if (this.rowHover) {
+                              clearTimeout(this.rowHover);
+                              this.rowHover = false;
+                            }
+                            this.rowHover = setTimeout(() => {
+                              this.setState({
+                                hover: item
+                              })
+                            }, 250);
+                          }}
+                          onMultiple={(selection) => {
+                            props.multipleSelected(
+                              selection, props.search.filter
+                            );
+                          }}
+                          onSelected={(borehole) => {
+                            // Lock borehole
+                            if (borehole !== null) {
+                              props.lock(borehole.id);
+                            }
+                          }}
+                        />
+                      </div>
+                      
+                    </div>
+                  )}
                 />
-              </div>
-            )}
-            path={process.env.PUBLIC_URL + "/editor/:id"}
-          />
-        </Switch>
-      </div>
-    </div>
-  );
+                <Route
+                  exact={false}
+                  path={process.env.PUBLIC_URL + "/editor/:id"}
+                  render={() => (
+                    <div
+                      style={{
+                        overflow: 'hidden',
+                        height: '100%',
+                        display: 'flex',
+                        flex: '1 1 100%',
+                        flexDirection: 'column',
+                        padding: '1em'
+                      }}
+                    >
+                      <BoreholeForm />
+                    </div>
+                  )}
+                />
+              </Switch>
+            </div>
+    
+            <Switch>
+              <Route 
+                component={({ match }) => (
+                  <div
+                    style={{
+                      width: '400px',
+                      boxShadow: 'rgba(0, 0, 0, 0.17) -2px 6px 6px 0px',
+                      padding: '1em'
+                    }}
+                  >
+                    <WorkflowForm
+                      id={
+                        parseInt(match.params.id, 10)
+                      }
+                    />
+                  </div>
+                )}
+                path={process.env.PUBLIC_URL + "/editor/:id"}
+              />
+            </Switch>
+          </div>
+        </div>
+      );
+    }
+
+  
   // }
 
 };
@@ -206,13 +289,14 @@ EditorComponent.propTypes = {
 
 const mapStateToProps = (state) => {
   return {
-    pcnt: state.core_project_list.dlen,
-    editor: state.core_borehole_editor_list,
     bcnt: state.core_borehole_list.dlen,
+    editor: state.core_borehole_editor_list,
+    pcnt: state.core_project_list.dlen,
     scnt: state.core_stratigraphy_list.dlen,
-    store: state.editor,
     search: state.searchEditor,
-    user: state.core_user
+    setting: state.setting,
+    store: state.editor,
+    user: state.core_user,
   };
 };
 
