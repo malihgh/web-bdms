06a06a2042636effafa981e98568a09dd11fb463
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2021-12-24 11:03:18 +0100

    🔥 feat : Creating buttons and give style to them in TranslationkKeys component.

--- src/commons/translationKeys/translationKeys.js
+++ src/commons/translationKeys/translationKeys.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import * as Styled from "./styles";
 
-const TranslationKeys = () => {
-  return <div>here is translation keys</div>;
+const TranslationKeys = (prop) => {
+  const languages = [
+    { id: 0, language: "de" },
+    { id: 1, language: "fr" },
+    { id: 2, language: "it" },
+    { id: 3, language: "en" },
+  ];
+  const [selectedLanguage, setSelectedLanguage] = useState(languages[3]);
+
+  return (
+    <Styled.Container>
+      {languages.map((item, key) => (
+        <Styled.Keys
+          key={key}
+          onClick={() => setSelectedLanguage(item)}
+          style={{
+            color:
+              selectedLanguage.language === item.language ? "red" : "black",
+            textDecoration:
+              selectedLanguage.language === item.language
+                ? "underline"
+                : "none",
+          }}
+        >
+          {item.language.toUpperCase()}
+        </Styled.Keys>
+      ))}
+    </Styled.Container>
+  );
 };
 
 export default TranslationKeys;
