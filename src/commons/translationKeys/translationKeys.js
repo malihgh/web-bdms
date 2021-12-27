283cf80787acbf7abfd93e63824364e534fca850
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2021-12-27 08:58:27 +0100

    feat : Add default value to TranslationKeys.

--- src/commons/translationKeys/translationKeys.js
+++ src/commons/translationKeys/translationKeys.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { composeInitialProps } from "react-i18next";
 import * as Styled from "./styles";
 
 const TranslationKeys = (prop) => {
-  const { handleSelectedLanguage } = prop;
+  const { handleSelectedLanguage, defaultLanguage } = prop;
+  const [selectedLanguage, setSelectedLanguage] = useState();
+
   const languages = [
     { id: 0, language: "de" },
     { id: 1, language: "fr" },
     { id: 2, language: "it" },
     { id: 3, language: "en" },
   ];
-  const [selectedLanguage, setSelectedLanguage] = useState(languages[3]);
+
+  useEffect(() => {
+    let lang;
+    switch (defaultLanguage) {
+      case "de":
+        lang = languages[0];
+        break;
+      case "fr":
+        lang = languages[1];
+        break;
+      case "it":
+        lang = languages[2];
+        break;
+      case "en":
+        lang = languages[3];
+    }
+    setSelectedLanguage(lang);
+  }, []);
 
   return (
     <Styled.Container>
@@ -22,11 +42,11 @@ const TranslationKeys = (prop) => {
           }}
           style={{
             color:
-              selectedLanguage.language === item.language ? "red" : "black",
-            textDecoration:
-              selectedLanguage.language === item.language
-                ? "underline"
-                : "none",
+              selectedLanguage?.language === item.language ? "red" : "black",
+            // textDecoration:
+            //   selectedLanguage?.language === item.language
+            //     ? "underline"
+            //     : "none",
           }}
         >
           {item.language.toUpperCase()}
