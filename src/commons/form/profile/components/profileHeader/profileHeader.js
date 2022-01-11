bf96493fb18bf2035ff09d12cb7ef2868f5c7894
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-01-11 12:36:02 +0100

    🔥 feat : Creating ProfileHeader component andd add buttons and styles.

--- src/commons/form/profile/components/profileHeader/profileHeader.js
+++ src/commons/form/profile/components/profileHeader/profileHeader.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
+import * as Styled from './styles';
+import { Button, Icon } from 'semantic-ui-react';
+import TranslationText from './../../../translationText';
 
 const ProfileHeader = () => {
-  return <div>Profile Header</div>;
-};
+  const [showStratigraphyButton, setShowStratigraphyButton] = useState(true);
+  const [isPrimary, setIsPrimary] = useState(true);
 
+  return (
+    <Styled.Container>
+      {showStratigraphyButton && (
+        <Button
+          content={<TranslationText id="stratigraphy" />}
+          icon="add"
+          secondary
+          size="small"
+        />
+      )}
+
+      <Styled.Item>
+        <Styled.ItemName>
+          {isPrimary && <Icon name="check" />}
+          Mali
+        </Styled.ItemName>
+        <Styled.ItemDate>1/02</Styled.ItemDate>
+      </Styled.Item>
+    </Styled.Container>
+  );
+};
 export default ProfileHeader;
