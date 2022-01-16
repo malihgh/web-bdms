54de2c9dfd46f0696f15e4965cc64d36bad22aa9
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-01-16 12:06:57 +0100

    feat : Update Profile component with new changes.

--- src/commons/form/profile/profile.js
+++ src/commons/form/profile/profile.js
@@ -1,36 +1,42 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import ProfileHeader from "./components/profileHeader";
-import ProfileLayers from "./components/profileLayers";
+import ProfileHeader from './components/profileHeader';
+import ProfileLayers from './components/profileLayers';
+import ProfileInfo from './components/profileInfo';
+import ProfileAttributes from './components/profileAttributes';
 
 // Take a look at the StratigraphyFormContainer
 
-const Profile = (props) => {
+const Profile = props => {
   return (
-    <div>
+    <div style={{ height: '100%' }}>
       <ProfileHeader />
       <div
         style={{
-          flex: "1 1 100%",
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <div>
-          <div>Profile info</div>
+          display: 'flex',
+          flexDirection: 'row',
+          padding: '10px',
+          paddingRight: '0px',
+          flex: 1,
+          height: '100%',
+        }}>
+        <div style={{ width: '60%' }}>
+          <ProfileInfo />
           <ProfileLayers />
         </div>
-        <div>Profile Attributes</div>
+        <div style={{ width: '40%' }}>
+          <ProfileAttributes />
+        </div>
       </div>
     </div>
   );
 };
 
 Profile.propTypes = {
-  id: PropTypes.number,
-  kind: PropTypes.number,
-  unlocked: PropTypes.bool,
+  // id: PropTypes.number,
+  // kind: PropTypes.number,
+  // unlocked: PropTypes.bool,
 };
 
 export default Profile;
