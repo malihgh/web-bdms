bf8837d838b0f9d3eed73ee9a66d6c0bca251bf4
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-01-17 14:30:02 +0100

    feat : Send data to ProfileHeader with Redux.

--- src/commons/form/profile/profile.js
+++ src/commons/form/profile/profile.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withTranslation } from 'react-i18next';
 
 import ProfileHeader from './components/profileHeader';
 import ProfileLayers from './components/profileLayers';
@@ -9,9 +12,16 @@ import ProfileAttributes from './components/profileAttributes';
 // Take a look at the StratigraphyFormContainer
 
 const Profile = props => {
+  const { data } = props.borehole;
+  const { data: user } = props.user;
+
+  const dataBorhole = {
+    data: props.borehole.data,
+    user: props.user.data,
+  };
   return (
     <div style={{ height: '100%' }}>
-      <ProfileHeader />
+      <ProfileHeader data={dataBorhole} />
       <div
         style={{
           display: 'flex',
@@ -34,9 +44,32 @@ const Profile = props => {
 };
 
 Profile.propTypes = {
+  borehole: PropTypes.object,
   // id: PropTypes.number,
   // kind: PropTypes.number,
   // unlocked: PropTypes.bool,
 };
 
-export default Profile;
+Profile.defaultProps = {
+  id: undefined,
+};
+
+const mapStateToProps = state => {
+  return {
+    borehole: state.core_borehole,
+    user: state.core_user,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    dispatch: dispatch,
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(withTranslation(['common'])(Profile)),
+);
