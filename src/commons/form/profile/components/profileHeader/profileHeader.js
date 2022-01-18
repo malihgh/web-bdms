73b83dafd4d1d2e965ab9227cd83dc32fa586b64
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-01-18 14:41:18 +0100

    feat : Send data to ProfileHeader and use data.

--- src/commons/form/profile/components/profileHeader/profileHeader.js
+++ src/commons/form/profile/components/profileHeader/profileHeader.js
@@ -1,32 +1,29 @@
-import React, { useState, useEffect } from "react";
-import * as Styled from "./styles";
-import { Button, Icon } from "semantic-ui-react";
-import TranslationText from "./../../../translationText";
-import PropTypes from "prop-types";
-import _ from "lodash";
-import DateText from "../../../dateText";
+import React, { useState, useEffect } from 'react';
+import * as Styled from './styles';
+import { Button, Icon } from 'semantic-ui-react';
+import TranslationText from './../../../translationText';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import DateText from '../../../dateText';
 
-const ProfileHeader = (props) => {
-  const { data, user } = props.data;
+const ProfileHeader = props => {
+  const { data, user, selectedStratigraphyID, setSelectedStratigraphyID } =
+    props.data;
 
   const [showStratigraphyButton, setShowStratigraphyButton] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(null);
 
   useEffect(() => {
     if (
       !(
         data.lock === null ||
         data.lock.username !== user.username ||
-        data.role !== "EDIT"
+        data.role !== 'EDIT'
       )
     ) {
       setShowStratigraphyButton(true);
     } else {
       setShowStratigraphyButton(false);
     }
-
-    if (data.stratigraphy && data.stratigraphy.length > 0)
-      setSelectedItem(data.stratigraphy[0].id);
   }, [props, setShowStratigraphyButton]);
 
   return (
@@ -41,19 +38,19 @@ const ProfileHeader = (props) => {
       )}
 
       {_.isArray(data.stratigraphy) &&
-        data.stratigraphy.map((item) => (
+        data.stratigraphy.map(item => (
           <Styled.Item
             key={item.id}
             onClick={() => {
-              setSelectedItem(item.id);
+              setSelectedStratigraphyID(item.id);
             }}
             style={{
-              borderBottom: item.id === selectedItem && "2px solid black",
-            }}
-          >
+              borderBottom:
+                item.id === selectedStratigraphyID && '2px solid black',
+            }}>
             <Styled.ItemName>
               {item.primary && <Icon name="check" />}
-              {item.name === null || item.name === "" ? "translate" : item.name}
+              {item.name === null || item.name === '' ? 'translate' : item.name}
             </Styled.ItemName>
             <Styled.ItemDate>
               <DateText date={item.date} />
