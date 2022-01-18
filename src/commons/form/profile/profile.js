7219426b09aaa557651a585fecb4d1f69b45eea8
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-01-18 17:00:46 +0100

    feat : Use hook in Profile and handle isEditable.

--- src/commons/form/profile/profile.js
+++ src/commons/form/profile/profile.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import * as Style from './styles';
-import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
 import ProfileHeader from './components/profileHeader';
 import ProfileLayers from './components/profileLayers';
@@ -13,41 +11,46 @@ import ProfileAttributes from './components/profileAttributes';
 // Take a look at the StratigraphyFormContainer
 
 const Profile = props => {
-  const [selectedItemID, setSelectedItemID] = useState(null);
+  const { user, borehole } = useSelector(state => ({
+    borehole: state.core_borehole,
+    user: state.core_user,
+  }));
+
   const [selectedStratigraphy, setSelectedStratigraphy] = useState(null);
+  const [isEditable, setIsEditable] = useState(false);
 
   useEffect(() => {
     if (
-      props.borehole.data.stratigraphy &&
-      props.borehole.data.stratigraphy.length > 0
+      !(
+        borehole?.data?.lock === null ||
+        borehole?.data?.lock.username !== user?.data?.username ||
+        borehole?.data?.role !== 'EDIT'
+      )
     ) {
-      setSelectedItemID(props.borehole.data.stratigraphy[0].id);
-      setSelectedStratigraphy(props.borehole.data.stratigraphy[0]);
+      setIsEditable(true);
+    } else {
+      setIsEditable(false);
     }
-  }, [props]);
+
+    if (borehole.data.stratigraphy && borehole.data.stratigraphy.length > 0) {
+      setSelectedStratigraphy(borehole.data.stratigraphy[0]);
+    }
+  }, [setIsEditable, borehole, user]);
+
+  // let selectedStratigraphy = useMemo(
+  //   () => {
+  //     return borehole?.data?.stratigraphy?.[0] ?? null;
+  // }, [borehole]);
 
   const dataBorhole = {
-    data: props.borehole.data,
-    user: props.user.data,
-    selectedStratigraphyID: selectedItemID,
-    setSelectedStratigraphyID: id => {
-      setSelectedItemID(id);
-
-      var elementPos = props.borehole.data.stratigraphy
-        .map(function (x) {
-          return x.id;
-        })
-        .indexOf(id);
-      setSelectedStratigraphy(props.borehole.data.stratigraphy[elementPos]);
-
-      console.log(
-        'hey',
-        props.borehole.data.stratigraphy,
-        selectedItemID,
-        selectedStratigraphy,
-      );
-    },
+    data: borehole.data,
+    user: user.data,
     selectedStratigraphy: selectedStratigraphy,
+    setSelectedStratigraphy: e => {
+      console.log('hey', e, isEditable);
+      setSelectedStratigraphy(e);
+    },
+    isEditable: isEditable,
   };
 
   return (
@@ -77,22 +80,4 @@ Profile.defaultProps = {
   id: undefined,
 };
 
-const mapStateToProps = state => {
-  return {
-    borehole: state.core_borehole,
-    user: state.core_user,
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    dispatch: dispatch,
-  };
-};
-
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  )(withTranslation(['common'])(Profile)),
-);
+export default Profile;
