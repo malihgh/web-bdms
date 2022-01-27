1076e72fe47dcd951300d492b649a2ae38dfd4ac
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-01-27 09:21:00 +0100

    🔥 feat : Create Cards for ProfileLayers.

--- src/commons/form/profile/components/profileLayers/profileLayers.js
+++ src/commons/form/profile/components/profileLayers/profileLayers.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import * as Styled from './styles';
 
 const ProfileLayers = () => {
-  return <div>ProfileLayers</div>;
+  return (
+    <Styled.Container>
+      <Styled.MyCard>
+        <Styled.CardPattern
+          bg={'#ff77ff'}
+          style={{ backgroundImage: `url('../../../../favicon-16x16.png')` }}>
+          pattern
+        </Styled.CardPattern>
+        <Styled.CardInfo>
+          <Styled.Text>0 m</Styled.Text>
+          <Styled.Text bold>
+            <Styled.DomainTxt
+              id={15201080}
+              schema={'custom.lit_str_top_bedrock'}
+            />
+          </Styled.Text>
+          <Styled.Text>
+            <Styled.DomainTxt
+              id={15001005}
+              schema={'custom.chro_str_top_bedrock'}
+            />
+          </Styled.Text>
+          <Styled.Text small>
+            <Styled.DomainTxt
+              id={15001005}
+              schema={'custom.chro_str_top_bedrock'}
+            />
+          </Styled.Text>
+
+          <Styled.Text>2 m</Styled.Text>
+        </Styled.CardInfo>
+        <Styled.CardDeleteButton>trash</Styled.CardDeleteButton>
+      </Styled.MyCard>
+    </Styled.Container>
+  );
 };
 
 export default ProfileLayers;
---
+++ src/commons/form/profile/components/profileLayers/styles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import DomainText from '../../../domain/domainText';
+export const Container = styled.div``;
+
+export const MyCard = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  border: 1px solid lightgrey;
+  border-radius: 4px;
+  flex: 1;
+`;
+
+export const CardPattern = styled.div`
+  background-color: ${props => props.bg};
+  background-image: url('../../../../15102007.svg');
+  background-size: cover;
+
+  flex: 1;
+  width: 40px;
+`;
+
+export const CardInfo = styled.div`
+  padding: 5px 0px;
+  padding-left: 10px;
+  flex: 5;
+`;
+
+export const CardDeleteButton = styled.div`
+  flex: 1;
+`;
+
+export const Text = styled.div`
+  font-weight: ${props => (props.bold ? 'bold' : '100')};
+  font-size: ${props => (props.bold ? '15px' : props.small ? '11px' : '13px')};
+  color: ${props => props.small && 'grey'};
+  padding-top: 3px;
+`;
+
+export const DomainTxt = styled(DomainText)``;
