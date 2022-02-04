7f3b6feb143bc0558b4da0dbd17c5d53177964bc
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-02-04 10:31:46 +0100

    feat : Add functions to handle changes in ProfileInfo.

--- src/commons/form/profile/components/profileInfo/profileInfo.js
+++ src/commons/form/profile/components/profileInfo/profileInfo.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './styles';
 import { Input, Form, Checkbox, Popup, Button, Icon } from 'semantic-ui-react';
 import TranslationText from '../../../translationText';
 import DateField from '../../../dateField';
+import { patchStratigraphy } from '@ist-supsi/bmsjs';
+
+import _ from 'lodash';
 
 const ProfileInfo = props => {
-  const { item, isEditable } = props.data;
+  const { item, isEditable, onUpdated } = props.data;
+  let updateAttributeDelay = {};
+  const [state, setState] = useState({
+    isFetching: false,
+    isPatching: false,
+  });
+  const updateChange = (attribute, value, to = true) => {
+    if (!isEditable) {
+      alert('You should press start editing button! ');
+      return;
+    }
 
+    setState(prevState => ({ ...prevState, isPatching: true }));
+    _.set(item, attribute, value);
+
+    patch(attribute, value, to);
+  };
+
+  const patch = (attribute, value, to = true) => {
+    if (
+      updateAttributeDelay.hasOwnProperty(attribute) &&
+      updateAttributeDelay[attribute]
+    ) {
+      clearTimeout(updateAttributeDelay[attribute]);
+      updateAttributeDelay[attribute] = false;
+    }
+    updateAttributeDelay[attribute] = setTimeout(
+      function () {
+        patchStratigraphy(item?.id, attribute, value)
+          .then(function (response) {
+            if (response.data.success) {
+              setState({ ...state, isPatching: false });
+              if (_.isFunction(onUpdated)) {
+                onUpdated(attribute);
+              }
+            } else {
+              alert(response.data.message);
+              window.location.reload();
+            }
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      },
+      to ? 500 : 0,
+    );
+  };
   return (
     <Styled.Container>
       <Styled.FormContainer>
@@ -25,7 +73,10 @@ const ProfileInfo = props => {
                 autoComplete="off"
                 autoCorrect="off"
                 spellCheck="false"
-                value={(item && item.name) || ''}
+                value={item?.name ?? ''}
+                onChange={e => {
+                  updateChange('name', e.target.value);
+                }}
               />
             </Form.Field>
 
@@ -37,7 +88,12 @@ const ProfileInfo = props => {
               <label>
                 <TranslationText id="date" />
               </label>
-              <DateField date={item && item.date} />
+              <DateField
+                date={item && item.date}
+                onChange={selected => {
+                  updateChange('date', selected, false);
+                }}
+              />
             </Form.Field>
           </Form.Group>
         </Form>
@@ -48,14 +104,24 @@ const ProfileInfo = props => {
           size="small"
           style={{
             display: 'flex',
+            paddingLeft: '5px',
           }}>
-          <Checkbox checked={item && item.primary} label="" toggle />
+          <Checkbox
+            checked={item && item.primary}
+            label=""
+            onChange={(ev, data) => {
+              if (data.checked === true) {
+                updateChange('primary', data.checked, false);
+              }
+            }}
+            toggle
+          />
           <TranslationText id="mainStratigraphy" />
         </Form>
         {isEditable && (
           <div style={{ display: 'flex' }}>
             <Button
-              // disabled={!_.isEmpty(this.state.consistency)}
+              // disabled={!_.isEmpty(state.consistency)}
               icon
               size="tiny">
               <Icon name="clone outline" />
