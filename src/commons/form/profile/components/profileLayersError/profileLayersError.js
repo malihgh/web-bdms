771c815387d94abf7e3a91f61b42d6940255b96e
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-03-04 11:38:35 +0100

    feat : Add dictionary for Type of Errors.

--- src/commons/form/profile/components/profileLayersError/profileLayersError.js
+++ src/commons/form/profile/components/profileLayersError/profileLayersError.js
@@ -1,33 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as Styled from './styles';
-import { Icon } from 'semantic-ui-react';
+import { Icon, Radio } from 'semantic-ui-react';
 import TranslationText from '../../../translationText';
 
 const ProfileLayersError = props => {
-  const { title, isEditable, id } = props.data;
+  const { title, isEditable, id, isInside } = props.data;
   const [showSolution, setShowSolution] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    let e;
+
+    switch (title) {
+      case 'missingBedrock':
+        e = ErrorTypes[0];
+        break;
+      case 'wrongDepth':
+        e = ErrorTypes[5];
+        break;
+      case 'invertedDepth':
+        e = ErrorTypes[1];
+        break;
+      case 'topOverlap':
+        e = ErrorTypes[2];
+        break;
+      case 'bottomOverlap':
+        e = ErrorTypes[2];
+        break;
+      case 'topDisjoint':
+        e = ErrorTypes[3];
+        break;
+      case 'bottomDisjoint':
+        e = ErrorTypes[3];
+        break;
+      case 'missingLayers':
+        e = ErrorTypes[4];
+        break;
+      default:
+        e = null;
+    }
+    setError(e);
+  }, [title]);
+  const ErrorTypes = [
+    {
+      id: 0,
+      messageId: 'errorMissingBedrock',
+      solutions: ['errorMissingBedrockSolution'],
+    },
+    {
+      id: 1,
+      messageId: 'invertedDepth',
+      solutions: [],
+    },
+    {
+      id: 2,
+      messageId: 'errorOverlap',
+      solutions: ['errorGapSolution2', 'errorGapSolution4'],
+    },
+    {
+      id: 3,
+      messageId: 'errorGap',
+      solutions: [
+        'errorGapSolution1',
+        'errorGapSolution2',
+        'errorGapSolution4',
+      ],
+    },
+    {
+      id: 4,
+      messageId: 'errorStartWrong',
+      solutions: ['errorGapSolution1', 'errorGapSolution3'],
+    },
+    { id: 5, messageId: 'errorWrongDepth', solutions: ['errorWrongDepth'] },
+  ];
 
   return (
-    <Styled.ErrorCard>
+    <Styled.ErrorCard
+      isFirstInList={error?.messageId === 'errorStartWrong'}
+      isInside={isInside}>
       <Styled.Row>
-        <div style={{ color: '#9f3a38' }}>
+        <Styled.ErrorMessageContainer>
           <Icon name="warning sign" />
-
-          {(title === 'topOverlap' || title === 'bottomOverlap') && (
-            <TranslationText id="errorOverlap" />
-          )}
-          {(title === 'topDisjoint' || title === 'bottomDisjoint') && (
-            <TranslationText id="errorGap" />
-          )}
-          {title === 'missingTo' && <>Add end of the layer</>}
-          {title === 'missingLayers' && (
-            <TranslationText id="errorStartWrong" />
-          )}
-          {title === 'missingBedrock' && (
-            <TranslationText id="errorMissingBedrock" />
-          )}
-          {title === 'invertedDepth' && <TranslationText id="invertedDepth" />}
-        </div>
+          {/* {title === 'missingTo' && <>Add end of the layer</>} */}
+          <TranslationText id={error?.messageId} />
+        </Styled.ErrorMessageContainer>
 
         {isEditable && showSolution !== id && (
           <Styled.CardButton
@@ -45,18 +101,25 @@ const ProfileLayersError = props => {
 
       {showSolution === id && (
         <div>
-          <div
-            style={{
-              fontSize: '0.8em',
-              color: '#9f3a38',
-            }}>
+          <Styled.HowToResolveContainer>
             <TranslationText id="errorHowToResolve" />
-          </div>
-          <div>sulutions</div>
-          <Styled.CardButtonContainer
-            style={{
-              justifyContent: 'flex-end',
-            }}>
+          </Styled.HowToResolveContainer>
+          {error?.solutions?.map((e, index) => (
+            <div key={index} style={{ marginTop: 2 }}>
+              {error.solutions.length > 1 && (
+                <Radio
+                  // checked={this.state.resolvingAction === 0}
+                  // label={e}
+                  name="radioGroup"
+                  // onChange={this.handleResolvingAction}
+                  value={0}
+                  style={{ marginRight: 4 }}
+                />
+              )}
+              <TranslationText id={e} />
+            </div>
+          ))}
+          <Styled.CardButtonContainer>
             <Styled.CardButton
               basic
               // color="red"
