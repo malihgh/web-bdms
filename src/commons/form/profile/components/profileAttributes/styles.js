661160ccb0f326d2251a15e2cce8b48619a5c34b
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-03-09 14:31:33 +0100

    💄 fix : Fix styles.

--- src/commons/form/profile/components/profileAttributes/styles.js
+++ src/commons/form/profile/components/profileAttributes/styles.js
@@ -4,7 +4,7 @@ import { Form } from 'semantic-ui-react';
 export const Container = styled.div`
   overflow-y: auto;
   flex: 1 1 100%;
-  padding: 0px 10px 0px 15px;
+  padding: 10px 10px 0px 15px;
   opacity: ${props => props.disable && '0.5'};
   pointer-events: ${props => props.disable && 'none'};
   background-color: ${props => !props.disable && 'lightgray'};
