35768f949db199bca0011c7cf64e080d2ce3b2ae
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-03-17 12:31:05 +0100

    feat : Add features to ProfileInstrument and change the style.

--- src/commons/form/profile/components/profileInstrument/profileInstrument.js
+++ src/commons/form/profile/components/profileInstrument/profileInstrument.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import * as Styled from './styles';
+import InstrumentList from './components/infoList/InstrumentList';
+import { attributes } from './data/attributes';
+import { Button } from 'semantic-ui-react';
+import TranslationText from '../../../translationText';
 
 const ProfileInstrument = () => {
-  return <Styled.Container>hey</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.ButtonContainer>
+        {/* {isEditable && ( */}
+        <Button
+          content={<TranslationText id="addInstrument" />}
+          icon="add"
+          //   onClick={CreateLayer}
+          secondary
+          size="tiny"
+          style={{ margin: 10 }}
+        />
+        {/* )} */}
+      </Styled.ButtonContainer>
+      <Styled.ListContainer>
+        <InstrumentList data={{ attributes, id: 0 }} />
+        <InstrumentList data={{ attributes, id: 1 }} />
+        <InstrumentList data={{ attributes, id: 1 }} />
+        <InstrumentList data={{ attributes, id: 1 }} />
+      </Styled.ListContainer>
+    </Styled.Container>
+  );
 };
 
 export default ProfileInstrument;
