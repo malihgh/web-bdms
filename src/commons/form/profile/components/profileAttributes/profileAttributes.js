b17cdefb0e6d30a91fca5d747cad8780cd6b03c0
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-05-11 16:09:57 +0200

    ♻️ refactor : Refactor ProfileAttributes with adding ProfileAttributeList.

--- src/commons/form/profile/components/profileAttributes/profileAttributes.js
+++ src/commons/form/profile/components/profileAttributes/profileAttributes.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import * as Styled from './styles';
-import { Checkbox, Input, TextArea, Form } from 'semantic-ui-react';
+import { Checkbox } from 'semantic-ui-react';
 import TranslationText from '../../../translationText';
-import DomainDropdown from '../../../domain/dropdown/domainDropdown';
-import DomainTree from '../../../domain/tree/domainTree';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 import { getData, sendAttribute } from './api';
+import ProfileAttributeList from './components/profileAttributeList/profileAttributeList';
 
 const ProfileAttributes = props => {
   const { id, isEditable, onUpdated, attribute, reloadAttribute } = props.data;
@@ -145,118 +144,11 @@ const ProfileAttributes = props => {
         <TranslationText id="showallfields" />
       </Styled.CheckboxContainer>
 
-      {attribute &&
-        attribute.map((item, key) => (
-          <Form autoComplete="false" error key={key}>
-            <Styled.AttributesContainer required={item.require}>
-              {(item.isVisible || showAll) && (
-                <Styled.Label>
-                  <TranslationText id={item.label} />
-                </Styled.Label>
-              )}
-              {item.type === 'Input' && (item.isVisible || showAll) && (
-                <Styled.AttributesItem>
-                  <Input
-                    autoCapitalize="off"
-                    autoComplete="off"
-                    autoCorrect="off"
-                    onChange={e =>
-                      updateChange(
-                        item.value,
-                        e.target.value === '' ? null : e.target.value,
-                        item?.to,
-                        item?.isNumber,
-                      )
-                    }
-                    spellCheck="false"
-                    style={{ width: '100%' }}
-                    value={
-                      _.isNil(state?.layer?.[item.value])
-                        ? ''
-                        : state.layer[item.value]
-                    }
-                  />
-                </Styled.AttributesItem>
-              )}
-
-              {item.type === 'TextArea' && (item.isVisible || showAll) && (
-                <Styled.AttributesItem>
-                  <TextArea
-                    onChange={e => updateChange(item.value, e.target.value)}
-                    style={{ width: '100%' }}
-                    value={
-                      _.isNil(state?.layer?.[item.value])
-                        ? ''
-                        : state.layer[item.value]
-                    }
-                  />
-                </Styled.AttributesItem>
-              )}
-
-              {item.type === 'Radio' && (item.isVisible || showAll) && (
-                <Form.Group style={{ display: 'flex', paddingTop: '5px' }}>
-                  <Form.Radio
-                    checked={
-                      _.isNil(state?.layer?.[item.value])
-                        ? false
-                        : state.layer[item.value]
-                    }
-                    label={'Yes'}
-                    onChange={() => updateChange(item.value, true, item?.to)}
-                    style={{ paddingRight: '20px' }}
-                  />
-                  <Form.Radio
-                    checked={
-                      _.isNil(state?.layer?.[item.value])
-                        ? false
-                        : !state.layer[item.value]
-                    }
-                    label={'No'}
-                    onChange={() => updateChange(item.value, false, item?.to)}
-                  />
-                </Form.Group>
-              )}
-
-              {item.type === 'Dropdown' && (item.isVisible || showAll) && (
-                <Styled.AttributesItem>
-                  <DomainDropdown
-                    multiple={item.multiple}
-                    onSelected={e =>
-                      updateChange(
-                        item.value,
-                        item.multiple ? e.map(mlpr => mlpr.id) : e.id,
-                        false,
-                      )
-                    }
-                    schema={item.schema}
-                    search={item.search}
-                    selected={
-                      _.isNil(state?.layer?.[item.value])
-                        ? null
-                        : state.layer[item.value]
-                    }
-                  />
-                </Styled.AttributesItem>
-              )}
-
-              {item.type === 'DomainTree' && (item.isVisible || showAll) && (
-                <Styled.AttributesItem>
-                  <DomainTree
-                    levels={item.levels}
-                    onSelected={e => updateChange(item.value, e.id, false)}
-                    schema={item.schema}
-                    selected={
-                      _.isNil(state?.layer?.[item.value])
-                        ? null
-                        : state.layer[item.value]
-                    }
-                    title={<TranslationText id={item.label} />}
-                  />
-                </Styled.AttributesItem>
-              )}
-            </Styled.AttributesContainer>
-          </Form>
-        ))}
+      {attribute && (
+        <ProfileAttributeList
+          data={{ attribute, showAll, updateChange, layer: state.layer }}
+        />
+      )}
     </Styled.Container>
   );
 };
