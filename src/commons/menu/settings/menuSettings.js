92907a6880fec5adeddbf987c9ae9314da3e4d6e
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-06-09 15:29:38 +0200

    feat (#8) : Fix background for selected Item in Setting menu.

--- src/commons/menu/settings/menuSettings.js
+++ src/commons/menu/settings/menuSettings.js
@@ -2,104 +2,74 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
-import {
-  withRouter
-} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-import {
-  List
-} from 'semantic-ui-react';
+import { List } from 'semantic-ui-react';
 
 import TranslationText from '../../form/translationText';
 
-
-const MenuSettings = (props) => {
-  const {
-    history,
-    t,
-    location
-  } = props;
+const MenuSettings = props => {
+  const { history, t, location } = props;
 
   return (
     <div>
-      <List
-        divided
-        relaxed
-        selection
-      >
+      <List divided relaxed selection>
         <List.Item
-          onClick={()=>{
-            history.push(
-              process.env.PUBLIC_URL + '/'
-            );
-          }}
-          style={{
-            padding: '1em'
-          }}
-        >
-          <List.Icon
-            name='arrow left'
-            size='large'
-            verticalAlign='middle'
-          />
-          <List.Content>
-            <List.Header as='h3'>
-              <TranslationText
-                id='done'
-              />
-            </List.Header>
-          </List.Content>
-        </List.Item>
-        <List.Item
-          onClick={()=>{
-            history.push(
-              process.env.PUBLIC_URL + '/setting/explorer'
-            );
+          active={location.pathname === process.env.PUBLIC_URL + '/'}
+          onClick={() => {
+            history.push(process.env.PUBLIC_URL + '/');
           }}
           style={{
             padding: '1em',
-            borderLeft: location.pathname.indexOf('/setting/explorer') >= 0?
-              '0.25em solid rgb(237, 29, 36)': null
-          }}
-        >
-          <List.Icon
-            name='binoculars'
-            size='large'
-            verticalAlign='middle'
-          />
+          }}>
+          <List.Icon name="arrow left" size="large" verticalAlign="middle" />
           <List.Content>
-            <List.Header as='h3'>
-              Viewer  
+            <List.Header as="h3">
+              <TranslationText id="done" />
             </List.Header>
           </List.Content>
         </List.Item>
-        {
-          props.user.data !== null
-          && props.user.data.roles.indexOf('EDIT') >= 0?
-            <List.Item
-              onClick={()=>{
-                history.push(
-                  process.env.PUBLIC_URL + '/setting/editor'
-                );
-              }}
-              style={{
-                padding: '1em',
-                borderLeft: location.pathname.indexOf('/setting/editor') >= 0?
-                  '0.25em solid rgb(237, 29, 36)': null
-              }}
-            >
-              <List.Icon
-                name='edit'
-                size='large'
-                verticalAlign='middle'
-              />
-              <List.Content>
-                <List.Header as='h3'>
-                  Editor  
-                </List.Header>
-              </List.Content>
-            </List.Item>: null
-        }
+        <List.Item
+          active={
+            location.pathname === process.env.PUBLIC_URL + '/setting/explorer'
+          }
+          onClick={() => {
+            history.push(process.env.PUBLIC_URL + '/setting/explorer');
+          }}
+          style={{
+            padding: '1em',
+            borderLeft:
+              location.pathname.indexOf('/setting/explorer') >= 0
+                ? '0.25em solid rgb(237, 29, 36)'
+                : null,
+          }}>
+          <List.Icon name="binoculars" size="large" verticalAlign="middle" />
+          <List.Content>
+            <List.Header as="h3">Viewer</List.Header>
+          </List.Content>
+        </List.Item>
+        {props.user.data !== null &&
+        props.user.data.roles.indexOf('EDIT') >= 0 ? (
+          <List.Item
+            active={
+              location.pathname === process.env.PUBLIC_URL + '/setting/editor'
+            }
+            onClick={() => {
+              history.push(process.env.PUBLIC_URL + '/setting/editor');
+            }}
+            style={{
+              padding: '1em',
+              borderLeft:
+                location.pathname.indexOf('/setting/editor') >= 0
+                  ? '0.25em solid rgb(237, 29, 36)'
+                  : null,
+            }}>
+            <List.Icon name="edit" size="large" verticalAlign="middle" />
+            <List.Content>
+              <List.Header as="h3">Editor</List.Header>
+            </List.Content>
+          </List.Item>
+        ) : null}
         {/* <List.Item
           onClick={()=>{
             history.push(
@@ -109,7 +79,7 @@ const MenuSettings = (props) => {
           style={{
             padding: '1em',
             borderLeft: location.pathname.indexOf('/setting/account') >= 0?
-              '0.5em solid rgb(237, 29, 36)': null
+              '0.25em solid rgb(237, 29, 36)': null
           }}
         >
           <List.Icon
@@ -123,134 +93,115 @@ const MenuSettings = (props) => {
             </List.Header>
           </List.Content>
         </List.Item> */}
-        {
-          props.user.data.admin === true?
-            <List.Item
-              onClick={()=>{
-                history.push(
-                  process.env.PUBLIC_URL + '/setting/admin'
-                );
-              }}
-              style={{
-                padding: '1em',
-                borderLeft: location.pathname.indexOf('/setting/admin') >= 0?
-                  '0.5em solid rgb(237, 29, 36)': null
-              }}
-            >
-              <List.Icon
-                name='user outline'
-                size='large'
-                verticalAlign='middle'
-              />
-              <List.Content>
-                <List.Header as='h3'>
-                  Admin
-                </List.Header>
-              </List.Content>
-            </List.Item>: null
-        }
-        {
-          props.user.data.admin === true?
-            <List.Item
-              onClick={()=>{
-                history.push(
-                  process.env.PUBLIC_URL + '/setting/term'
-                );
-              }}
-              style={{
-                padding: '1em',
-                borderLeft: location.pathname.indexOf('/setting/term') >= 0?
-                  '0.5em solid rgb(237, 29, 36)': null
-              }}
-            >
-              <List.Icon
-                name='pencil'
-                size='large'
-                verticalAlign='middle'
-              />
-              <List.Content>
-                <List.Header as='h3'>
-                  {t('common:terms')}
-                </List.Header>
-              </List.Content>
-            </List.Item>: null
-        }
-        {
-          props.user.data.admin === true?
-            <List.Item
-              onClick={()=>{
-                history.push(
-                  process.env.PUBLIC_URL + '/setting/login'
-                );
-              }}
-              style={{
-                padding: '1em',
-                borderLeft: location.pathname.indexOf('/setting/login') >= 0?
-                  '0.5em solid rgb(237, 29, 36)': null
-              }}
-            >
-              <List.Icon
-                name='sign-in'
-                size='large'
-                verticalAlign='middle'
-              />
-              <List.Content>
-                <List.Header as='h3'>
-                  {t('common:loginScreen')}
-                </List.Header>
-              </List.Content>
-            </List.Item>: null
-        }
-        {
-          props.user.data !== null &&
-          (
-            props.user.data.admin === true
-          )?
-            <List.Item
-              onClick={()=>{
-                history.push(
-                  process.env.PUBLIC_URL + '/setting/database'
-                );
-              }}
-              style={{
-                padding: '1em',
-                borderLeft: location.pathname.indexOf('/setting/database') >= 0?
-                  '0.5em solid rgb(237, 29, 36)': null
-              }}
-            >
-              <List.Icon
-                name='database'
-                size='large'
-                verticalAlign='middle'
-              />
-              <List.Content>
-                <List.Header as='h3'>
-                  Database
-                </List.Header>
-              </List.Content>
-            </List.Item>: null
-        }
+        {props.user.data.admin === true ? (
+          <List.Item
+            active={
+              location.pathname === process.env.PUBLIC_URL + '/setting/admin'
+            }
+            onClick={() => {
+              history.push(process.env.PUBLIC_URL + '/setting/admin');
+            }}
+            style={{
+              padding: '1em',
+              borderLeft:
+                location.pathname.indexOf('/setting/admin') >= 0
+                  ? '0.25em solid rgb(237, 29, 36)'
+                  : null,
+            }}>
+            <List.Icon
+              name="user outline"
+              size="large"
+              verticalAlign="middle"
+            />
+            <List.Content>
+              <List.Header as="h3">Admin</List.Header>
+            </List.Content>
+          </List.Item>
+        ) : null}
+        {props.user.data.admin === true ? (
+          <List.Item
+            active={
+              location.pathname === process.env.PUBLIC_URL + '/setting/term'
+            }
+            onClick={() => {
+              history.push(process.env.PUBLIC_URL + '/setting/term');
+            }}
+            style={{
+              padding: '1em',
+              borderLeft:
+                location.pathname.indexOf('/setting/term') >= 0
+                  ? '0.25em solid rgb(237, 29, 36)'
+                  : null,
+            }}>
+            <List.Icon name="pencil" size="large" verticalAlign="middle" />
+            <List.Content>
+              <List.Header as="h3">{t('common:terms')}</List.Header>
+            </List.Content>
+          </List.Item>
+        ) : null}
+        {props.user.data.admin === true ? (
+          <List.Item
+            active={
+              location.pathname === process.env.PUBLIC_URL + '/setting/login'
+            }
+            onClick={() => {
+              history.push(process.env.PUBLIC_URL + '/setting/login');
+            }}
+            style={{
+              padding: '1em',
+              borderLeft:
+                location.pathname.indexOf('/setting/login') >= 0
+                  ? '0.25em solid rgb(237, 29, 36)'
+                  : null,
+            }}>
+            <List.Icon name="sign-in" size="large" verticalAlign="middle" />
+            <List.Content>
+              <List.Header as="h3">{t('common:loginScreen')}</List.Header>
+            </List.Content>
+          </List.Item>
+        ) : null}
+        {props.user.data !== null && props.user.data.admin === true ? (
+          <List.Item
+            active={
+              location.pathname === process.env.PUBLIC_URL + '/setting/database'
+            }
+            onClick={() => {
+              history.push(process.env.PUBLIC_URL + '/setting/database');
+            }}
+            style={{
+              padding: '1em',
+              borderLeft:
+                location.pathname.indexOf('/setting/database') >= 0
+                  ? '0.25em solid rgb(237, 29, 36)'
+                  : null,
+            }}>
+            <List.Icon name="database" size="large" verticalAlign="middle" />
+            <List.Content>
+              <List.Header as="h3">Database</List.Header>
+            </List.Content>
+          </List.Item>
+        ) : null}
         <List.Item
-          onClick={()=>{
-            history.push(
-              process.env.PUBLIC_URL + '/setting/resources'
-            );
+          active={
+            location.pathname === process.env.PUBLIC_URL + '/setting/resources'
+          }
+          onClick={() => {
+            history.push(process.env.PUBLIC_URL + '/setting/resources');
           }}
           style={{
             padding: '1em',
-            borderLeft: location.pathname.indexOf('/setting/resources') >= 0?
-              '0.5em solid rgb(237, 29, 36)': null
-          }}
-        >
+            borderLeft:
+              location.pathname.indexOf('/setting/resources') >= 0
+                ? '0.25em solid rgb(237, 29, 36)'
+                : null,
+          }}>
           <List.Icon
-            name='linkify' // barcode
-            size='large'
-            verticalAlign='middle'
+            name="linkify" // barcode
+            size="large"
+            verticalAlign="middle"
           />
           <List.Content>
-            <List.Header as='h3'>
-              {t('common:resources')}
-            </List.Header>
+            <List.Header as="h3">{t('common:resources')}</List.Header>
           </List.Content>
         </List.Item>
       </List>
@@ -265,33 +216,31 @@ MenuSettings.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string,
   }).isRequired,
-  t: PropTypes.func
+  t: PropTypes.func,
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     setting: state.setting,
-    user: state.core_user
+    user: state.core_user,
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     dispatch: dispatch,
-    boreholeSelected: (borehole) => {
+    boreholeSelected: borehole => {
       dispatch({
         path: '/borehole',
-        type: 'CLEAR'
+        type: 'CLEAR',
       });
-    }
+    },
   };
 };
 
 export default withRouter(
   connect(
     mapStateToProps,
-    mapDispatchToProps
-  )(
-    withTranslation(['common'])(MenuSettings)
-  )
+    mapDispatchToProps,
+  )(withTranslation(['common'])(MenuSettings)),
 );
