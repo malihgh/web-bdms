232100c4ec0b862766f2455b54770e752648728c
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-06-10 09:21:06 +0200

    feat : Create ListItem to show the list of setting.

--- src/commons/menu/components/listItem/styles.js
+++ src/commons/menu/components/listItem/styles.js
@@ -1,3 +1,10 @@
 import styled from 'styled-components';
+import { List } from 'semantic-ui-react';
 
 export const Container = styled.div``;
+
+export const Hr = styled.div`
+  border-top-width: 1px;
+  border-top-style: solid;
+  border-top-color: #e0e0e0;
+`;
