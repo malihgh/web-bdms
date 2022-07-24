d2c8a3465eb40aec0ff65ed108648ffef31f0f4f
Maliheh <malih.ghasemian@gmail.com>
2022-07-24 13:29:30 +0200

    feat (#61) : Add code for handlingList in EditorSettingList.

--- src/pages/settings/components/editorSettingList/editorSettingList.js
+++ src/pages/settings/components/editorSettingList/editorSettingList.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import * as Styled from './styles';
+import { Checkbox, Segment } from 'semantic-ui-react';
+import TranslationText from '../../../../commons/form/translationText';
 
-const EditorSettingList = () => {
-  return <Styled.Container>EditorSettingList</Styled.Container>;
+const EditorSettingList = props => {
+  const { setting, toggleFilter, attribute } = props;
+
+  return (
+    <>
+      <Segment.Group>
+        {attribute.map((item, index) => (
+          <Segment key={index}>
+            <Checkbox
+              checked={
+                item.value.split('.').length > 1
+                  ? setting.data.efilter?.[item.value.split('.')[0]]?.[
+                      item.value.split('.')[1]
+                    ]
+                  : setting.data.efilter?.[item.value]
+              }
+              label=""
+              onChange={(e, d) => {
+                toggleFilter(item.value, d.checked);
+              }}
+            />
+            <TranslationText id={item.label} />
+          </Segment>
+        ))}
+      </Segment.Group>
+    </>
+  );
 };
 
 export default EditorSettingList;
