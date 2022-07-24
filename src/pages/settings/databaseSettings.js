044c237d01a6d02fc206f04ee13f4062577879c4
Maliheh <malih.ghasemian@gmail.com>
2022-07-24 11:06:13 +0200

    feat (#60) : Add description for Export and Import Database.

--- src/pages/settings/databaseSettings.js
+++ src/pages/settings/databaseSettings.js
@@ -224,20 +224,23 @@ class DatabaseSettings extends React.Component {
             flexDirection: 'row',
             display: 'flex',
           }}>
-          <Header
-            as="h3"
-            className="link"
-            onClick={() => {
-              this.setState({
-                export: !this.state.export,
-              });
-            }}
-            style={{
-              margin: '0px',
-              textDecoration: 'none',
-            }}>
-            <TranslationText id="export" />
-          </Header>
+          <div>
+            <Header
+              as="h3"
+              className="link"
+              onClick={() => {
+                this.setState({
+                  export: !this.state.export,
+                });
+              }}
+              style={{
+                margin: '0px',
+                textDecoration: 'none',
+              }}>
+              <TranslationText id="export" />
+            </Header>
+            <TranslationText id="export_database" />
+          </div>
           <div
             style={{
               flex: 1,
@@ -363,20 +366,23 @@ class DatabaseSettings extends React.Component {
             flexDirection: 'row',
             display: 'flex',
           }}>
-          <Header
-            as="h3"
-            className="link"
-            onClick={() => {
-              this.setState({
-                restore: !this.state.restore,
-              });
-            }}
-            style={{
-              margin: '0px',
-              textDecoration: 'none',
-            }}>
-            Import
-          </Header>
+          <div>
+            <Header
+              as="h3"
+              className="link"
+              onClick={() => {
+                this.setState({
+                  restore: !this.state.restore,
+                });
+              }}
+              style={{
+                margin: '0px',
+                textDecoration: 'none',
+              }}>
+              Import
+            </Header>
+            <TranslationText id="import_database" />
+          </div>
           <div
             style={{
               flex: 1,
