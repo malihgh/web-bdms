200a1edacabb4e4d409adec527f72c06d8adb5da
Maliheh <malih.ghasemian@gmail.com>
2022-07-24 12:09:59 +0200

    feat (#61) : Create EditorSettingList for EditorSetting component.

---
+++ src/pages/settings/components/editorSettingList/editorSettingList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './styles';
+
+const EditorSettingList = () => {
+  return <Styled.Container>EditorSettingList</Styled.Container>;
+};
+
+export default EditorSettingList;
