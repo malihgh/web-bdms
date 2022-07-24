611e4f5c150a2b7a7a44b1f2b94aa47c3ecf0f55
Maliheh <malih.ghasemian@gmail.com>
2022-07-24 13:29:53 +0200

    feat (#61) : Add BoreholeEditorData.

---
+++ src/pages/settings/data/boreholeEditorData.js
@@ -0,0 +1,117 @@
+export const boreholeEditorData = [
+  {
+    id: '0',
+    label: 'kind',
+    value: 'kind',
+  },
+  {
+    id: '1',
+    label: 'drilling_method',
+    value: 'extended.method',
+  },
+  {
+    id: '2',
+    label: 'purpose',
+    value: 'extended.purpose',
+  },
+  {
+    id: '3',
+    label: 'cuttings',
+    value: 'custom.cuttings',
+  },
+  {
+    id: '4',
+    label: 'spud_date',
+    value: '',
+  },
+  {
+    id: '5',
+    label: 'drilling_end_date',
+    value: 'drilling_date',
+  },
+  {
+    id: '6',
+    label: 'drill_diameter',
+    value: 'custom.drill_diameter',
+  },
+  {
+    id: '7',
+    label: 'boreholestatus',
+    value: 'extended.status',
+  },
+  {
+    id: '7',
+    label: 'inclination',
+    value: 'bore_inc',
+  },
+  {
+    id: '8',
+    label: 'inclination_direction',
+    value: '',
+  },
+  {
+    id: '9',
+    label: 'qt_bore_inc_dir',
+    value: '',
+  },
+  {
+    id: '10',
+    label: 'totaldepth',
+    value: 'length',
+  },
+  {
+    id: '11',
+    label: 'total_depth_tvd',
+    value: '',
+  },
+  {
+    id: '12',
+    label: 'top_bedrock',
+    value: 'extended.top_bedrock',
+  },
+  {
+    id: '13',
+    label: 'top_bedrock_tvd',
+    value: '',
+  },
+  {
+    id: '14',
+    label: 'qt_depth',
+    value: '',
+  },
+  {
+    id: '15',
+    label: 'total_depth_tvd_qt',
+    value: '',
+  },
+  {
+    id: '16',
+    label: 'qt_top_bedrock',
+    value: '',
+  },
+  {
+    id: '17',
+    label: 'top_bedrock_tvd_qt',
+    value: '',
+  },
+  {
+    id: '18',
+    label: 'groundwater',
+    value: 'extended.groundwater',
+  },
+  {
+    id: '19',
+    label: 'lithology_top_bedrock',
+    value: 'custom.lit_pet_top_bedrock',
+  },
+  {
+    id: '20',
+    label: 'lithostratigraphy_top_bedrock',
+    value: 'custom.lit_str_top_bedrock',
+  },
+  {
+    id: '21',
+    label: 'chronostratigraphy_top_bedrock',
+    value: 'custom.chro_str_top_bedrock',
+  },
+];
