b74587f6936925d1412ea3866e52b16b40df2c75
Maliheh <malih.ghasemian@gmail.com>
2022-08-05 12:07:57 +0200

    feat (#40) : Remove top and botton from StratigraphyFieldEditorData.

--- src/pages/settings/data/stratigraphyFieldEditorData.js
+++ src/pages/settings/data/stratigraphyFieldEditorData.js
@@ -2,156 +2,146 @@ export const stratigraphyFieldEditorData = [
   // value has problem and they are not working
   {
     id: 0,
-    label: 'layer_depth_from',
-    value: 'depth_from', //////
-  },
-  {
-    id: 1,
-    label: 'layer_depth_to',
-    value: 'depth_to', /////
-  },
-  {
-    id: 2,
     label: 'lithological_description',
     value: '', //////
   },
   {
-    id: 3,
+    id: 1,
     label: 'facies_description',
     value: '', /////
   },
   {
-    id: 4,
+    id: 2,
     label: 'layer_last',
     value: '', /////
   },
   {
-    id: 5,
+    id: 3,
     label: 'layer_qt_description',
     value: 'qt_description',
   },
   {
-    id: 6,
+    id: 4,
     label: 'layer_lithology',
     value: 'lithology',
   },
   {
-    id: 7,
+    id: 5,
     label: 'layer_lithostratigraphy',
     value: 'lithostratigraphy',
   },
   {
-    id: 8,
+    id: 6,
     label: 'layer_chronostratigraphy',
     value: 'chronostratigraphy',
   },
   {
-    id: 9,
+    id: 7,
     label: 'layer_uscs_original',
     value: 'uscs_original',
   },
   {
-    id: 10,
+    id: 8,
     label: 'layer_uscs_determination',
     value: 'uscs_determination',
   },
   {
-    id: 11,
+    id: 9,
     label: 'layer_uscs_1',
     value: 'uscs_1',
   },
   {
-    id: 12,
+    id: 10,
     label: 'layer_grain_size_1',
     value: 'grain_size_1',
   },
   {
-    id: 13,
+    id: 11,
     label: 'layer_uscs_2',
     value: 'uscs_2',
   },
   {
-    id: 14,
+    id: 12,
     label: 'layer_grain_size_2',
     value: 'grain_size_2',
   },
   {
-    id: 15,
+    id: 13,
     label: 'layer_uscs_3',
     value: 'uscs_3',
   },
   {
-    id: 16,
+    id: 14,
     label: 'layer_grain_shape',
     value: 'grain_shape',
   },
   {
-    id: 17,
+    id: 15,
     label: 'layer_grain_granularity',
     value: 'grain_granularity',
   },
   {
-    id: 18,
+    id: 16,
     label: 'layer_organic_component',
     value: 'organic_component',
   },
   {
-    id: 19,
+    id: 17,
     label: 'layer_debris',
     value: 'debris',
   },
   {
-    id: 20,
+    id: 18,
     label: 'layer_lithology_top_bedrock',
     value: 'lithology_top_bedrock',
   },
   {
-    id: 21,
+    id: 19,
     label: 'layer_striae',
     value: 'striae',
   },
   {
-    id: 22,
+    id: 20,
     label: 'layer_color',
     value: 'color',
   },
   {
-    id: 23,
+    id: 21,
     label: 'layer_consistance',
     value: 'consistance',
   },
   {
-    id: 24,
+    id: 22,
     label: 'layer_plasticity',
     value: 'plasticity',
   },
   {
-    id: 25,
+    id: 23,
     label: 'layer_compactness',
     value: 'compactness',
   },
   {
-    id: 26,
+    id: 24,
     label: 'layer_cohesion',
     value: 'cohesion',
   },
   {
-    id: 27,
+    id: 25,
     label: 'gradation',
     value: '', ///
   },
   {
-    id: 28,
+    id: 26,
     label: 'layer_humidity',
     value: 'humidity',
   },
   {
-    id: 29,
+    id: 27,
     label: 'layer_alteration',
     value: 'alteration',
   },
   {
-    id: 30,
+    id: 28,
     label: 'layer_notes',
     value: 'notes', ///is not working
   },
