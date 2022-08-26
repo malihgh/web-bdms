c088ccb27ce2b844ba1514ca9fcb70d221e451ed
Maliheh <malih.ghasemian@gmail.com>
2022-08-26 10:40:27 +0200

    🐛 feat : Fix warning for MemoryLeak in UseCasingList.

--- src/commons/form/profile/hooks/useCasingList.js
+++ src/commons/form/profile/hooks/useCasingList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { profileKind } from '../constance';
 import { useTranslation } from 'react-i18next';
 import { getProfile } from '../components/profileInstrument/api';
@@ -21,21 +21,30 @@ export default function useCasingList(boreholeID) {
     { key: 1, value: 0, text: t('common:no_casing') },
   ];
   const [casing, setCasing] = useState([]);
+  const mounted = useRef(false);
 
   useEffect(() => {
+    mounted.current = true;
+
     getProfile(boreholeID, profileKind.CASING).then(response => {
       const temp = initialCasing;
       if (response.length > 0) {
-        for (const e of response) {
+        response.forEach(e => {
           temp.push({
             key: e.id,
             value: e.id,
             text: e.name === null || e.name === '' ? t('common:np') : e.name,
           });
-        }
+        });
       }
-      setCasing(temp);
+      if (mounted.current) {
+        setCasing(temp);
+      }
     });
+
+    return () => {
+      mounted.current = false;
+    };
   }, [boreholeID, t, initialCasing]);
 
   return { casing };
